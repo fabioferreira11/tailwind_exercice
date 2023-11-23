2bbac1283ee21fe1010811888567a30538485fb7
fabioferreira11 <145472440+fabioferreira11@users.noreply.github.com>
2023-11-23 14:16:25 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'purple': '#755CDE',
+        'orange': '#F6A560',
+        'rose': '#F39E9E',
+        'red': '#EB7565',
+        'green': '#61C4B7',
+        'brown': '#552049',
+        'black': '#030303',
+        'gray': '#7A746E',
+        'beige': '#FFF7F0',
+      },
+      fontFamily: {
+        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      fontSize: {
+        large: ['56px'],
+        medium: ['40px'],
+        saml: ['32px'],
+        xsmal: ['18px'],
+      },
+      fontWeight: {
+        title: ['700'],
+        texte: ['500'],
+      },
+    },
+    
+   
+
+  },
+  plugins: [],
+}
+
